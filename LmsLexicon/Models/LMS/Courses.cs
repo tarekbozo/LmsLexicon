3edeea192e6e5897a5b0b3d10bd03e60536a1448
Tarek Bouzo <tarekbozo@gmail.com>
2017-08-15 11:43:19 +0200

    add Models

---
+++ LmsLexicon/Models/LMS/Courses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LmsLexicon.Models.LMS
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public byte NumberOfDays { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}

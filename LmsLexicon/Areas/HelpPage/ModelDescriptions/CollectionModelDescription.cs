5e75c24b550144ad8b83e29a69d2875cf232fc77
Tarek Bouzo <tarekbozo@gmail.com>
2017-08-15 11:25:54 +0200

    Add project files.

---
+++ LmsLexicon/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace LmsLexicon.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ LmsLexicon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LmsLexicon.WebApiApplication" Language="C#" %>
